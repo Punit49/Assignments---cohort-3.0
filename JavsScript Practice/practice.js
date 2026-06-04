7ed864ca6c4ddaefeae904b76d85c0a46dd133ab
Punit49 <therealpunit5@gmail.com>
2026-06-04 22:42:12 +0530

    Assignment 3 Design - 2

---
+++ JavsScript Practice/practice.js
@@ -0,0 +1,28 @@
+// ! JavaScript Practice Task – Beginner Friendly
+
+// ? Part 1: Variables, Functions & Conditions
+
+// * Q1. Create a function that returns the sum of two numbers.
+    function add(a, b){
+        return a + b;
+    }
+
+    const res1 = add(10, 5);
+    console.log(res1); // Output: 15
+
+// * Q2. Create a function that returns the square of a number.
+    function square(n){
+        return n * n;
+    }
+
+    const res2 = square(5);
+    console.log(res2); // Output: 25
+
+// * Q3. Create a function that checks whether a number is Even or Odd.
+    function checkEvenOdd(n){
+        if(n % 2 === 0) return true;
+        if(n % 2 !== 0) return false;
+    }
+
+    const res3 = square(5);
+    console.log(res3); // Output: false
